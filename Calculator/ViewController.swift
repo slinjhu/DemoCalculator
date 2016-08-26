e1e87007b4c3beb91c13812a6c787d93443b2971
Sen Lin <slin@jhu.edu>
2016-08-26 00:57:30 -0400

    Using a calculated variable to hold display data, which will automatically update the lable text.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -5,9 +5,19 @@ class ViewController: UIViewController {
  
     @IBOutlet weak var display: UILabel!
     
+    var displayValue : Double {
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
     @IBAction func touchDigit(sender: UIButton) {
-        let digit : Double = Double(sender.currentTitle!)!
-        display.text = String(digit)
+        let digit = Double(sender.currentTitle!)!
+
+        displayValue = digit
     }
 
 }
